560b0e7031f43ba00fc759175612ac53cd20ad3f
Leon Graff <442graff@gmail.com>
2023-07-22 01:42:02 -0300

    terminando 3a questao

--- precos petshop.py
+++ precos petshop.py
@@ -34,7 +34,7 @@ def cachorro_peso():
             if(peso_dog < 3):
                 print("O peso do cachorro não pode ser menor do que 3kg.");
             elif(peso_dog > 50):
-                print("O peso do cachorro não pode ser maior do que 50kg.");
+                print("Não aceitamos cachorros tão grandes.");
             else:
                 preco_peso = obterPreco(peso_dog,pesos_precos);  
                 print(preco_peso);
@@ -51,15 +51,19 @@ def cachorro_pelo():
     mult = 0
     
     while True:
+        print("Entre com o pelo do cachorro");
+        print("c - Pelo curto");
+        print("m - Pelo médio");
+        print("l - Pelo longo");
         try:
             pelo_dog = input("Digite o tipo de pelo do seu cachorro: ")
-            if pelo_dog.lower() == "curto":
+            if pelo_dog == "c":
                 mult = pelo_curto
                 break  # Se o valor for válido, encerra o loop e retorna o multiplicador
-            elif pelo_dog.lower() == "médio" or pelo_dog.lower() == "medio":
+            elif pelo_dog == "m":
                 mult = pelo_medio
                 break  # Se o valor for válido, encerra o loop e retorna o multiplicador
-            elif pelo_dog.lower() == "longo":
+            elif pelo_dog) == "l":
                 mult = pelo_longo
                 break  # Se o valor for válido, encerra o loop e retorna o multiplicador
             else:
@@ -70,7 +74,33 @@ def cachorro_pelo():
     print(mult)
     return mult  # Imprime o multiplicador válido
 
+def cachorro_extra():
+    extra = 0;
+    while True:
+        print("Deseja adicionar mais algum serviço?");
+        print("1 - Corte de unhas - R$ 10,00");
+        print("2 - Escovar dentes - R$ 12,00");
+        print("3 - Limpeza de orelhas - R$ 15,00");
+        print("0 - Não desejo mais nada");
+        try:
+            serv_adc = int(input);
+            if serv_adc in (1,2,3,0):
+                if serv_adc == 1:
+                    extra += 10; 
+                elif serv_adc == 2:
+                    extra += 12; 
+                elif serv_adc == 3:
+                    extra += 15; 
+                elif serv_adc == 0:
+                    extra += 0; 
+                    break;
+            if serv_adc not in(1,2,3,0):
+                print("Por favor digite um valor válido.")
+        except ValueError: 
+            print("Por favor digite um valor válido.");
+
 
 
 cachorro_peso();
 cachorro_pelo();
+cachorro_extra();
