912f2d266c3945173588318f7b8652384c2954ef
Leon Graff <442graff@gmail.com>
2023-07-25 00:39:04 -0300

    finalizado

--- software gerenc-pessoas.py
+++ software gerenc-pessoas.py
@@ -1,65 +1,102 @@
-# dicionarios em python
-"""
-comportamento semelhante ao de objetos em javascript
-colaborador = {
-nome: joao,
-setor: manutenção,
-salario: 2500
+# A. Print de boas-vindas com o seu nome
+print("Bem-vindo ao sistema de cadastro de colaboradores do Leon Graff!")
 
-}
-
-"""
+# criando id global e a lista
 lista_colaboradores = []
 id_global = 0
 
 
+# definindo função de cadastro
 def cadastrar_colaborador(id):
-    print("xx")
+    global id_global  # Use the global id_global variable
+    id_global = id
+    nome = input("Digite o nome do colaborador: ")
+    setor = input("Digite o setor do colaborador: ")
+    salario = float(input("Digite o salário do colaborador: "))
+
+    colaborador = {"id": id, "nome": nome, "setor": setor, "salário": salario}
+
+    # adicionando dicionario a lista
+    lista_colaboradores.append(colaborador)
 
 
-## nome id e salario
-"""
-C.	Deve-se criar uma função chamada cadastrar_colaborador(id) 
-em que: [EXIGÊNCIA DE CÓDIGO 2 de 7];
-a.	Pergunta nome, setor, pagamento do colaborador;
-b.	Armazena o id (este é fornecido via parâmetro da função), 
-nome, setor, salário dentro de um dicionário;
-c.	Copiar o dicionário dentro para dentro da da 
-lista_colaboradores;
- 
-"""
+# definindo função de consulta de colaborador
+def consultar_colaborador():
+    opcao = int(
+        input(
+            "Digite a opção desejada:\n1. Consultar Todos os colaboradores\n2. Consultar por Id\n3. Consultar por Setor\n4. Retornar ao menu\n"
+        )
+    )
+
+    if opcao == 1:
+        # consulta todos
+        print("------------ MENU CONSULTAR COLABORADOR ------------")
+        for colaborador in lista_colaboradores:
+            for key, value in colaborador.items():
+                print("{}: {}".format(key, value))
+            print()
+    elif opcao == 2:
+        # consulta por id
+        id_consulta = int(input("Digite o id do colaborador a ser consultado: "))
+        for colaborador in lista_colaboradores:
+            if colaborador["id"] == id_consulta:
+                print("----- Colaborador encontrado -----")
+                for key, value in colaborador.items():
+                    print("{}: {}".format(key, value))
+                print()
+                break
+        else:
+            print("Colaborador não encontrado.")
+    elif opcao == 3:
+        # por setor
+        setor_consulta = input("Digite o setor dos colaboradores a serem consultados: ")
+        print("----- Colaboradores do Setor {} -----".format(setor_consulta))
+        for colaborador in lista_colaboradores:
+            if colaborador["setor"].lower() == setor_consulta.lower():
+                for key, value in colaborador.items():
+                    print("{}: {}".format(key, value))
+                print()
+    elif opcao == 4:
+        return
+    else:
+        print("Opção inválida.")
 
 
-def consultar_colaborador(id):
-    print("xx")
+# definindo função para remover colaborador
+def remover_colaborador():
+    id_remocao = int(input("Digite o id do colaborador a ser removido: "))
+    for colaborador in lista_colaboradores:
+        if colaborador["id"] == id_remocao:
+            lista_colaboradores.remove(colaborador)
+            print("Colaborador removido com sucesso.")
+            break
+    else:
+        print("Colaborador não encontrado.")
 
 
-## 1. Consultar Todos / 2. Consultar por Id / 3. Consultar por Setor
-#  / 4. Retornar ao menu) e   realizar o print “Opção inválida"
-# se entrar com valor diferente de 1, 2, 3 ou 4
-
-
-def remover_colaborador(id):
-    # pergunta o id e remove o colaborador
-    print("xx")
-
-
+# função main
 def main():
-    print("main")
+    global id_global
+    while True:
+        print("\n-------------- MENU PRINCIPAL --------------")
+        print("1. Cadastrar Colaborador")
+        print("2. Consultar Colaborador(es)")
+        print("3. Remover Colaborador")
+        print("4. Sair")
+        opcao_menu = int(input("Escolha uma opção: "))
+
+        if opcao_menu == 1:
+            id_global += 1
+            cadastrar_colaborador(id_global)
+        elif opcao_menu == 2:
+            consultar_colaborador()
+        elif opcao_menu == 3:
+            remover_colaborador()
+        elif opcao_menu == 4:
+            print("Encerrando o programa.")
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
 
 
-"""
-F.	Deve-se criar uma estrutura de menu no main em que: 
-a.	Deve-se pergunta qual opção deseja (1. Cadastrar Colaborador / 
-2. Consultar Colaborador / 3. Remover Colaborador / 
-4. Encerrar Programa) e realizar o print “Opção inválida" 
-se entrar com valor diferente de 1, 2, 3 ou 4 :
-i.	Se Cadastrar Colaborador, acrescentar em um a variavel id_ global
- e chamar a função cadastrar_colaborador(id_ global);
-ii.	Se Consultar Colaborador, chamar função consultar_colaborador();
-iii.	Se Remover Colaborador, chamar função remover_colaborador();
-iv.	Se Encerrar Programa, sair do menu (e com isso acabar a execução
- do código);
-
-
-"""
+main()
